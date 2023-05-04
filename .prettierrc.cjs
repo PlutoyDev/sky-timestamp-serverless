15593e17b309d8f60a687d458ab2a5cdd5f38583
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2023-05-04 16:35:02 +0800

    project init

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  arrowParens: 'avoid',
+  printWidth: 120,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  quoteProps: 'consistent',
+};
